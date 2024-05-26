c3bddf355e12546752ba7c103dfc07dd0c56d489
Siorjiddin <sirojiddinyusupov55@gmail.com>
2024-05-26 18:12:05 +0500

    git fork

---
+++ script.js
@@ -0,0 +1,40 @@
+var name = prompt(" Ismingizni kiriting ")
+console.log(" Your name " + name);
+
+var age = +prompt( " Yoshingizni kiriting " )
+console.log(" Your age "  + age );
+
+var num = +prompt( "210 + 90=?" )
+if ( num >= 300 ) {
+    console.log(" Sizning javob togri 300 ");
+}else {
+    console.log( " Sizning javob natogri  Togri javob esa 300 ");
+}
+
+var num = +prompt( "500 - 400=?" )
+if ( num >=100) {
+    console.log( " Sizning javob togri 100 " );
+}else{
+    console.log( "Sizning javob natogri togri javob 100" );
+}
+
+var num = +prompt( "100*10=?" )
+if ( num >=1000) {
+    console.log( " Sizning javob togri 1000 " );
+}else{
+    console.log( " Sizning javob natogri togri javob 1000 " );
+}
+
+var num = +prompt( "30/2=?" )
+if ( num >=15) {
+    console.log( " Sizning javob togri 15 " );
+}else{
+    console.log( " Sizning javob natogri togri javob 15 " );
+}
+
+var num = +prompt( "25%3=?" )
+if ( num >=1) {
+    console.log( " Sizning javob togri 1 " );
+}else{
+    console.log( " Sizning javob natogri togri javob 1 " );
+}
